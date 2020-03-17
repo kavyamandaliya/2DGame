3b49b2ee50702fe5b53e07580eee485d1f6cf1b6
Kavya Mandaliya <kavya@kavyas-air.lan>
2020-03-17 14:13:29 -0700

    added physics

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-2, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(2, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            rb.velocity = new Vector2(0, -2);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0, 2);
+        }
+    }
+    
+}
