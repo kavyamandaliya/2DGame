3122e427bfcfdc1ab5e6f0defaf36cc6641a7210
Kavya Mandaliya <kavya@kavyas-air.lan>
2020-03-17 18:44:28 -0700

    player updates

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Collider2D coll;
+    [SerializeField]private LayerMask ground;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+    private void Update()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+    
+}
